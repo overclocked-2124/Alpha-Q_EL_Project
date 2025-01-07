b158a233ab24690027915b0fc05ae6d2c56b689d
Sumukha_Upadhyaya <146051934+overclocked-2124@users.noreply.github.com>
2025-01-07 10:36:33 +0530

    removed unwanted files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify ,request
+from flask import Flask, render_template, jsonify 
 from flask_sqlalchemy import SQLAlchemy
 from models import db, SensorData
 import random
