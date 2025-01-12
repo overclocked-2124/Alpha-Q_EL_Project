409821cbfe91e339721799bcfc521f0f57d8185f
Sumukha_Upadhyaya <146051934+overclocked-2124@users.noreply.github.com>
2025-01-12 19:14:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sensor_data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
+
 # Read CSV data
 df = pd.read_csv('solar_teg_dataset.csv')
 
